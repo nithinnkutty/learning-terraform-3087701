9a7954966bc2e8af12875c3c6168220c65add913
Nithin Narayanan Kutty <128687194+nithinnkutty@users.noreply.github.com>
2023-10-25 15:39:58 +0100

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -2,3 +2,39 @@ variable "instance_type" {
   description = "Type of EC2 instance to provision"
   default     = "t3.nano"
 }
+
+variable "ami_filter" {
+  description = "Name filter and owner for AMI"
+  type = object({
+    name  = string
+    owner = string
+  })
+  default = {
+    name    = "bitnami-tomcat-*-x86_64-hvm-ebs-nami"
+    owner   = "979382823631" # Bitnami
+  }
+}
+
+variable "environment" {
+  description = "development environment"
+  type = object({
+    name             = string
+    network_prefix   = string
+  })
+  default = {
+    name             = "dev"
+    network_prefix   = "10.0"
+  }
+}
+
+variable "min_size" {
+  description = "Min number of instanaces in ASG"
+  default = 1
+}
+
+variable "max_size" {
+  description = "Max number of instanaces in ASG"
+  default = 2
+}
+
+
