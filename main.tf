8812d51c87b716db9ccb570e336a504884bb75e9
Nithin Narayanan Kutty <128687194+nithinnkutty@users.noreply.github.com>
2023-10-25 12:50:03 +0100

    Add autoscaling group

--- main.tf
+++ main.tf
@@ -33,17 +33,22 @@ module "blog_vpc" {
   }
 }
 
-resource "aws_instance" "blog" {
-  ami           = data.aws_ami.app_ami.id
+module "autoscaling" {
+  source  = "terraform-aws-modules/autoscaling/aws"
+  version = "7.1.0"
+
+  name = "blog"
+  min_size = 1
+  max_size = 2
+
+  vpc_zone_identifier = module.blog_vpc.public_subnets
+  target_group_arns   = moduke.blog_alb.target_group_arns
+  security_groups     = [module.blog_sg.security_group_id]
+  
+  image_id           = data.aws_ami.app_ami.id
   instance_type = var.instance_type
 
-  vpc_security_group_ids = [module.blog_sg.security_group_id]
 
-  subnet_id    = module.blog_vpc.public_subnets[0]
-
-  tags = {
-    Name = "Learning Terraform"
-  }
 }
 
 module "alb" {
